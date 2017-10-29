56dc7ac86a5d114081663ec53d34c352449678a2
haaz <haaz77@gmail.com>
2017-10-29 14:13:44 +0300

    test

--- templates/demo/0/sources/utils/django.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def import_django_settings():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "_project_.settings")
-    import django; django.setup()  # noqa
-    from django.conf import settings  # noqa
-    return settings
